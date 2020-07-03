7eb6ca2ce1ac2af7c5383dfcdaaeb3a216ff2a33
Štěpán Škorpil <stepan@skorpil.cz>
2020-07-03 22:21:13 +0200

    Updated to redesigned pages

--- application/src/top.ts
+++ application/src/top.ts
@@ -6,6 +6,8 @@ test('Make top', async (t): Promise<number> => {
   console.info('Logging in');
   await t
     .wait(2000)
+    .click('.right > a.rootmenu')
+    .wait(2000)
     .click('a[href="/uzivatel/prihlaseni"]')
     .wait(2000)
     .typeText('#tiLoginUsernameModal', process.env.USERNAME)
@@ -13,7 +15,12 @@ test('Make top', async (t): Promise<number> => {
     .click('#btnLoginModal')
     .wait(2000);
   console.info('Entering goods list');
-  await t.click('a[href="/moje-zbozi?start=1"]').wait(2000);
+  await t
+    .wait(2000)
+    .hover('.hoverable > a.rootmenu.first')
+    .wait(2000)
+    .click('a[href="/moje-zbozi2"]')
+    .wait(2000);
 
   const randomGoodsPage = Selector((): Node[] => {
     const getRandomInt = (min: number, max: number): number => {
@@ -21,7 +28,7 @@ test('Make top', async (t): Promise<number> => {
       const maxInt = Math.floor(max);
       return Math.floor(Math.random() * (maxInt - minInt + 1)) + minInt;
     };
-    const pageButtonElements = document.querySelectorAll('center table tbody tr td div center a');
+    const pageButtonElements = document.querySelectorAll('#panel-bottom .numerics > a');
     const index = getRandomInt(0, pageButtonElements.length);
     if (index === pageButtonElements.length) {
       return [];
@@ -30,35 +37,50 @@ test('Make top', async (t): Promise<number> => {
   });
   console.info('Entering random goods page');
   await t.click(randomGoodsPage).wait(2000);
-  const randomTopButton = Selector((): Node[] => {
+
+  const randomGoodsItem = Selector((): Node[] => {
     const getRandomInt = (min: number, max: number): number => {
       const minInt = Math.ceil(min);
       const maxInt = Math.floor(max);
       return Math.floor(Math.random() * (maxInt - minInt + 1)) + minInt;
     };
-    const pageButtonElements = document.querySelectorAll('a[title="Topovat"]');
-    if (pageButtonElements.length === 0) {
+    const editButtonElements = document.querySelectorAll(
+      '.product .cartbuttons [fler-action="open_edit_panel"]'
+    );
+    if (editButtonElements.length === 0) {
       return [];
     }
-    const index = getRandomInt(0, pageButtonElements.length - 1);
-    return [pageButtonElements[index]];
+    const index = getRandomInt(0, editButtonElements.length - 1);
+    return [editButtonElements[index]];
+  });
+  console.info('Selecting random goods item');
+  await t.click(randomGoodsItem).wait(2000);
+
+  const topButton = Selector((): Node[] => {
+    const topButtons = document.querySelectorAll(
+      '.flypanel.visible .buttonmenu [data-editblock-load="topproduct"]'
+    );
+    if (topButtons.length === 0) {
+      return [];
+    }
+    return [topButtons[0]];
   });
   console.info('Trying to top');
-  const canBeTopped = await randomTopButton.exists;
+  const canBeTopped = await topButton.exists;
   if (canBeTopped) {
     console.info('Topping good');
     await t
       .setNativeDialogHandler((type, text, url): boolean => {
         return true;
       })
-      .click(randomTopButton)
+      .click(topButton)
       .wait(2000);
   } else {
     console.info('Missing top button');
   }
   console.info('Parsing waiting time');
-  const topTimer = Selector('.mzbozi_bound table tbody tr td center div small');
-  const topTimerText = await topTimer.innerText;
+  const topTimer = Selector('.flypanel.visible .info.message span');
+  const topTimerText = await topTimer.getAttribute('title');
   const matches = topTimerText.match(/(\d{2}:\d{2}:\d{2})/g);
   const timeSegments = matches[0].split(':');
   const seconds =
